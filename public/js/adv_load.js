41f96812acd7d1cebd048c7ef2ce1b319216c11b
James Jarvis <jamespjarvis@gmail.com>
2017-06-04 13:45:21 -0400

    added logos and stuff

---
+++ public/js/adv_load.js
@@ -0,0 +1,36 @@
+(function(d) {
+
+  const config = {
+    kitId: 'ctb0phy',
+    scriptTimeout: 3000,
+    async: true
+  };
+
+  let h = d.documentElement;
+  let t = setTimeout(function() { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout);
+  let tk = d.createElement("script");
+  let f = false;
+  let s = d.getElementsByTagName("script")[0];
+  let a;
+
+  h.className += " wf-loading";
+
+  tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
+
+  tk.async = true;
+
+  tk.onload = tk.onreadystatechange = function() {
+    a = this.readyState;
+    if (f || a && a != "complete" && a != "loaded") return;
+    f = true;
+    clearTimeout(t);
+    try {
+      Typekit.load(config);
+    } catch (e) {
+      console.log('e');
+    }
+  };
+
+  s.parentNode.insertBefore(tk, s);
+
+})(document);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET users page. */
+router.get('/', (req, res, next) => {
+  res.send('Respond with resource');
 });
 
 module.exports = router;
