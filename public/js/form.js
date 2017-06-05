dacb1a9e72c77d0bfa192a2d7a7217ac594355e5
James Jarvis <jamespjarvis@gmail.com>
2017-06-05 15:09:04 -0400

    added header and base styles

--- public/js/form.js
+++ public/js/form.js
@@ -3,16 +3,72 @@ function Form() {
     this.submitButton = document.querySelector('button.submit');
 
     this.form = document.querySelector('form#contact');
-
     this.name = document.querySelector('input#name');
     this.company = document.querySelector('input#company');
     this.email = document.querySelector('input#email');
     this.message = document.querySelector('textarea#message');
 
+    this.formInputs = this.form.querySelectorAll('.form-input');
+    this.formInputs.forEach(input => input.addEventListener('change', this.formInputHandler));
+    this.formInputs.forEach(input => input.addEventListener('keyup', this.formInputHandler));
+
     this.submitButton.addEventListener('click', this.submitForm);
+
+    this.formReady = false;
+    this.formSubmitted = false;
+  }
+  this.formInputHandler = (e) => {
+    this.formSwitch(e.target);
+    this.checkFormSubmitReady();
+    if (this.formReady) {
+      this.submitButton.disabled = false;
+    }
+  }
+  this.checkFormSubmitReady = () => {
+    if (this.form.querySelectorAll('.success').length === 4) {
+      this.submitButton.disabled = false;
+    } else {
+      this.submitButton.disabled = true;
+    }
+  }
+  this.formSwitch = (target) => {
+    let id = target.id;
+    let value = target.value;
+    let valid = false;
+    switch (id) {
+      case 'name':
+        if (value.split(' ').length > 1) valid = true;
+        break;
+      case 'company':
+        valid = true;
+        break;
+      case 'email':
+        const regex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (regex.test(value)) valid = true;
+        break;
+      case 'message':
+        if (value.length) valid = true;
+        break;
+      default:
+        console.log('default!');
+        break;
+    }
+    return this.modifyElementStatus(target, valid);
+  }
+  this.modifyElementStatus = (target, valid) => {
+    if (valid) {
+      target.classList.remove('error');
+      target.classList.add('success');
+      target.removeAttribute('required');
+    } else {
+      target.classList.add('error');
+    }
+    return valid;
   }
   this.submitForm = (e) => {
     e.preventDefault();
+    if (this.formSubmitted) return;
+    this.formSubmitted = true;
     const formData = {
       name: this.name.value,
       company: this.company.value,
@@ -25,7 +81,7 @@ function Form() {
       data: JSON.stringify(formData)
     }
 
-    requestify('/api', options).then(JSON.parse).then(res => {
+    this.requestify('/api', options).then(JSON.parse).then(res => {
       if (res.message === 'SUCCESS') {
         this.displayResultMessage('&#10003;', 'rgb(90, 191, 13)');
       } else {
@@ -57,24 +113,24 @@ function Form() {
     success.appendChild(check);
     document.body.appendChild(success);
   }
+
+  this.requestify = (url, options) => {
+    return new Promise((resolve, reject) => {
+      const req = new XMLHttpRequest();
+      req.open(options.method, url);
+      if (typeof options.headers !== 'undefined') options.headers.forEach(header => req.setRequestHeader(header.name, header.value));
+      req.onload = () => {
+        if (req.status >= 200 && req.status < 300) {
+          resolve(req.response);
+        } else {
+          reject(Error(req.statusText));
+        }
+      }
+      req.onerror = () => reject({ status: req.status, statusText: req.statusText });
+      req.send(options.data);
+    });
+  }
 }
 
 const form = new Form();
 form.initialize();
-
-function requestify(url, options) {
-  return new Promise((resolve, reject) => {
-    const req = new XMLHttpRequest();
-    req.open(options.method, url);
-    if (typeof options.headers !== 'undefined') options.headers.forEach(header => req.setRequestHeader(header.name, header.value));
-    req.onload = () => {
-      if (req.status >= 200 && req.status < 300) {
-        resolve(req.response);
-      } else {
-        reject(Error(req.statusText));
-      }
-    }
-    req.onerror = () => reject({ status: req.status, statusText: req.statusText });
-    req.send(options.data);
-  });
-}
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,16 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: `Home - Gordo Gustavo's` });
 });
 
+/* GET about page. */
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: `About - Gordo Gustavo's` });
+});
+
+/* GET services page. */
+router.get('/services', (req, res, next) => {
+  res.render('services', { title: `Services - Gordo Gustavo's` });
+});
+
 /* GET contact page. */
 router.get('/contact', (req, res, next) => {
   res.render('contact', { title: `Contact - Gordo Gustavo's` });
