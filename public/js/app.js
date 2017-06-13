828796f21319c4dedfe8c912585197bfda87ce3d
James Jarvis <jamespjarvis@gmail.com>
2017-06-13 15:54:32 -0400

    bug fixes

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,9 @@ function App() {
     this.navIcon = document.querySelector('.nav-icon');
     this.nav.style.height = '0%';
     this.navToggle.addEventListener('click', this.toggleNavPageVisibility);
+
+    window.addEventListener('resize', this.resizeWindow);
+    this.resizeWindow();
   }
   this.toggleNavPageVisibility = () => {
     if (this.nav.style.height === '0%') {
@@ -17,6 +20,9 @@ function App() {
       this.nav.style.height = '0%';
       this.navToggle.classList.remove('open');
     }
+  }
+  this.resizeWindow = () => {
+    window.innerWidth = window.outerWidth;
   }
 }
 
